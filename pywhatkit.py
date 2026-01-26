81e1c16b64984d7268cf8874fc2be93e2f1629d0
HemanthReddyBandi <hemanth.in22@gmail.com>
2026-01-26 14:15:01 +0530

    initial

--- pywhatkit.py
+++ pywhatkit.py
@@ -2,7 +2,10 @@ import pyautogui
 import time
 time.sleep(7)
 count=0
-while count<=20:
-    pyautogui.typewrite("good night😴🌜")
+while count<=2:
+    pyautogui.typewrite("ekkada vunnav")
     pyautogui.press ("enter")
+    
+    # for linkedin send button
+    #pyautogui.hotkey("ctrl", "enter") 
     count=count+1
